af8047f14ffdb7f0be78826a066c618318997ef2
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-03-07 14:33:29 +0000

    add init test
    add init test

---
+++ tests/basic_boot.rs
@@ -0,0 +1,31 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os_study::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os_study::test_panic_handler(info)
+}
+
+use blog_os_study::{println, serial_print, serial_println};
+#[test_case]
+fn test_println() {
+    serial_print!("testing_println...\n");
+    println!("this is test");
+    serial_println!("[ok]");
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
