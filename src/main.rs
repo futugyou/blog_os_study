a81383f6911e1e23af10a0d14c4012a0b3d6b633
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-03-07 10:50:24 +0000

    VGA Text Mode
    VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,34 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello world!";
+//static HELLO: &[u8] = b"Hello world!";
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}",info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // let vga_buffer = 0xb8000 as *mut u8;
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+
+    //vga_buffer::print_someting();
+
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello world 2333").unwrap();
+    write!(vga_buffer::WRITER.lock(),", this si {} and {}",42,10./3.0).unwrap();
+    println!();
+    println!("hahahaha {} 233","OJBK!");
+    panic!("this si panic!");
     loop {}
 }
+
+
+mod vga_buffer;
